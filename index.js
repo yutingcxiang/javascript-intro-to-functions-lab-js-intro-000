a6394055bda1b762ad9143aa0bd996ecad47fa1c
Christine Xiang <yutingcxiang@gmail.com>
2018-06-14 00:32:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === "I love you, too") {
-    return "I love you, Grandma";
+  if (string === "I love you, Grandma") {
+    return "I love you, too";
   } else if (string.toLowerCase() === true) {
     return "I can't hear you!";
   } else if (string.toUpperCase() === true) {
