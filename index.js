2fab6ab2dd40e0de2e487aa2a7294bd15965696a
Christine Xiang <yutingcxiang@gmail.com>
2018-06-14 00:29:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string) {
+  if (lowercase.toLowerCase() === true) {
+    return "I can't hear you!";
+  }
+}
